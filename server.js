c20d2c2022675cba64a2d2649928504653272621
henrywisner8 <henrywisner8@gmail.com>
2025-05-10 21:33:54 -0500

    Add survey routes

--- server.js
+++ server.js
@@ -13,3 +13,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+const surveyRoutes = require('./routes/surveyRoutes');
+app.use('/api/surveys', surveyRoutes);
