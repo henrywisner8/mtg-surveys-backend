6fdc13e2d7241485c9bbc7ee595684a51e91253f
henrywisner8 <henrywisner8@gmail.com>
2025-06-18 21:56:24 -0500

    Update code and config

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ app.use(cors({
 }));
 
 
+process.on('uncaughtException', (err) => {
+  console.error('❌ Uncaught Exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('❌ Unhandled Rejection:', reason);
+  process.exit(1);
+});
+
+
 app.use(express.json());
 
 // Route imports
